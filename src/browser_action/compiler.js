a4694f89d4615bdebb64280d49e56085aa97b725
Ryan Rice <ryanr1230@gmail.com>
2014-11-18 09:49:25 -0600

    using requirejs

---
+++ src/browser_action/compiler.js
@@ -0,0 +1,15 @@
+requirejs.config({
+    baseUrl: '../../manatee-scraper-js/ManaTEAMS',
+});
+
+require(['sha1', 'jquery-2.1.1.min', 'TEAMSParser'],
+    function(Sha1, $, TEAMSParser) {
+        require(['classes/Assignment',
+            'classes/Category',
+            'classes/ClassGrades',
+            'classes/Semester',
+            'classes/Course',
+            'classes/Cycle',
+            'ManaTEAMS'
+        ]);
+    });
